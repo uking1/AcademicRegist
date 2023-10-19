e9b0b1480a1d7c6741b92b54b685937d5af65b05
Lee Jeong Uk <uking1@naver.com>
2023-10-19 15:02:41 +0900

    [create MenuView]

---
+++ academicProject/src/View/MenuView.java
@@ -0,0 +1,68 @@
+package View;
+
+import java.util.Scanner;
+
+public class MenuView {
+	public static Scanner scan = new Scanner(System.in);
+	
+	
+	// 메인 메뉴
+	public static void mainMenuView() {
+		System.out.println("미래대학교 수강신청 프로그램");
+		System.out.println("해당 번호를 입력하세요.");
+		System.out.println("1. 학과정보 목록/입력/수정/삭제");
+		System.out.println("2. 학생정보 목록/입력/수정");
+		System.out.println("3. 과목정보 목록/입력/수정/삭제");
+		System.out.println("4. 수강신청 목록/신청/취소");
+		System.out.println("5. 프로그램 종료");
+		System.out.print("번호 선택 >>");
+	}	
+		
+	// 학과 메뉴
+		
+	public static void subjectMenuView() {
+		System.out.println("학과정보 메뉴 번호를 입력하세요.");
+		System.out.println("1. 학과정보 목록");
+		System.out.println("2. 학과정보 입력");
+		System.out.println("3. 학과정보 수정");
+		System.out.println("4. 학과정보 삭제");
+		System.out.println("5. 메인 메뉴");
+		System.out.print("번호 선택 >>");
+	}
+	
+	// 학생 메뉴
+	public static void studentMenuView() {
+		System.out.println("학생 정보 메뉴 번호를 입력하세요.");
+		System.out.println("1. 학생 정보 목록");
+		System.out.println("2. 학생 정보 입력");
+		System.out.println("3. 학생 정보 수정");
+		System.out.println("4. 메인 메뉴");
+		System.out.print("번호 선택 >>");
+		
+	}
+	
+	
+	// 과목 메뉴
+	public static void lessonMenuView() {
+		System.out.println("과목 정보 메뉴 번호를 입력하세요.");
+		System.out.println("1. 과목 정보 목록");
+		System.out.println("2. 과목 정보 입력");
+		System.out.println("3. 과목 정보 수정");
+		System.out.println("4. 과목 정보 삭제");
+		System.out.println("5. 메인 메뉴");
+		System.out.print("번호 선택 >>");
+	}
+	
+	// 수강신청 메뉴
+	public static void traineeMenuView() {
+		System.out.println("수강 정보 메뉴 번호를 입력하세요.");
+		System.out.println("1. 수강신청 목록");
+		System.out.println("2. 수강 신청 ");
+		System.out.println("3. 수강 신청 취소");
+		System.out.println("4. 수강 신청 메뉴");
+		System.out.print("번호 선택 >>");
+		
+	}
+	
+	
+}
